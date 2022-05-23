335c24a6c0fbfa6b2789f58e94613f6e8ec1c4d2
Sagar <sutarsagarsanjay@gmail.com>
2022-05-23 06:59:55 +0530

    Minor comment addition

--- 02-ReplacingDefineMacro.cpp
+++ 02-ReplacingDefineMacro.cpp
@@ -7,6 +7,9 @@
  * Defining a Constant Pointer.
  * Both "pointer" and "value that pointer point to" is declared const
  * because more often than not, this header file will be included in many source files.
+ *
+ * Figure out why can't we do the same for integer i.e. const int *const ASPECT_RATIO = 12; ??
+ * Refer: https://stackoverflow.com/questions/18048140/int-and-const-int-initialization-and-conversion-in-c
  */
 const char *const ASPECT_RATIO = "12";
 
