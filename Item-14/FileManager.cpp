ef1e17f479c4b9a4da9f5dd1a69c2a93357fcfe6
Sagar S Sutar <ssutar@deeptrekker.com>
2023-10-23 09:17:04 +0530

    Improved the Client, Server & FileManager

--- Item-14/FileManager.cpp
+++ Item-14/FileManager.cpp
@@ -4,23 +4,43 @@
 #include <memory>
 #include <chrono>
 #include <thread>
+#include <cstring>
 using namespace std;
 
+/**
+ * File: A File Manager class. Handles safe creation, ownership transfer & deletion of the fstream object.
+*/
 class File{
 public:
-    File(std::string filePath, std::ios_base::openmode operationMode = std::ios::out) :
+    File(std::string filePath, std::ios_base::openmode operationMode = std::ios_base::in | std::ios_base::out) :
+    m_operationMode(operationMode),
     m_filePath(filePath),
     m_filePtr(std::shared_ptr<fstream>(new fstream(filePath, operationMode), fileDeleter))
     {
-        cout << "File: Default Constructor called. " << this << endl;
+        cout << "xxx \t " << std::ios::in << "\n";
+        cout << "xxx \t " << std::ios::out << "\n";
+        cout << "xxx \t " << m_filePtr.get()->is_open() << "\n";
+        cout << "File: Default Constructor called. " << this;
+        cout << " | fstream created " << m_filePtr.get() << endl;
     }
-
-    // Ownership transfered
-    File(const File &file){
-         cout << "File: Copy Constructor called. " << this << endl;
+    
+    //!< \brief Copy Constructor
+    File(const File &file){        
+        cout << "File: Copy Constructor called. " << this;
         m_filePath = file.m_filePath;
+        m_operationMode = file.m_operationMode;
+
+        // Shallow copy: Here the ownership of file is transferred.
+        // In O/P, you will notice only one file object gets deleted once the program ends.
         // m_filePtr.reset(file.m_filePtr.get());
-        m_filePtr = std::shared_ptr<ifstream>(new ifstream(m_filePath), fileDeleter);
+
+        // Deep Copy: We make a new copy of the file object.
+        // In O/P, you will see two file objects gets deleted, one that this class holds 
+        // & another the parent oject which was used during the copy construction.
+        std::shared_ptr<fstream> fileCopy = std::shared_ptr<fstream>(new fstream(m_filePath, m_operationMode), fileDeleter);
+        m_filePtr.swap(fileCopy);
+        
+        cout << " | fstream created " << m_filePtr.get() << endl;
     }
 
     // Disallow assignment.
@@ -30,16 +50,19 @@ public:
         cout << "File: Destructor called. " << this << endl;
     }
 
+    //!< \brief Custom deleter for the fstream object.
     static void fileDeleter(fstream *file){ 
-        cout << "File: Custom ifstream deleter called." << endl;        
+        cout << "File: Custom ifstream deleter called. " << file << endl;        
         if(file->is_open())
             file->close();       
         delete file;
     }
 
+    //!< \brief Basic write function.
     void write(std::string data){
         if(isOpen()){
             m_filePtr.get()->write(data.c_str(), data.length());
+            cout << "File: Written " << data.size() << " bytes to " << m_filePath << endl;
         }
         else{
             cout << "File: Can't write to a closed file.\n";
@@ -49,8 +72,8 @@ public:
     //!< \brief Returns a byte vector of the file contents.
     std::vector<char> fileToByteVec(){
         if(isOpen()){
-            int fileSize = getSize() ;
-            char buffer[fileSize];
+            int fileSize = getSize();
+            char buffer[fileSize];            
             m_filePtr.get()->read(buffer, fileSize);
             return std::vector<char>(buffer, buffer + fileSize);
         }
@@ -86,12 +109,38 @@ public:
 private:
     std::shared_ptr<fstream> m_filePtr;
     std::string m_filePath;
+    std::ios_base::openmode m_operationMode;
 };
 
-int main(){
-    cout << "0\n";
-    File f("random.txt");
-    File f1 = f;
-    cout << "2\n";
-    return 0;
-}
+
+/**============= Following are testing function for the FileManager class =============*/
+/**
+ * \brief In this function we test whether the File's shared pointer keep the underlying fstream alive.
+*/
+// void randonFunction(File &obj){
+//     cout << "randonFunction() called with " << &obj << endl;
+//     std::this_thread::sleep_for(std::chrono::seconds(5));
+//     cout << "randonFunction() ended.\n";
+// }
+
+// /**
+//  * \brief In this function we test the ownership/copy construction behaviour of the File.
+// */
+// void basicTest(){
+//     cout << "basicTest() called.\n";
+//     File f("/home/ssutar/Tutorials/Effective-CPP/Item-14/random.txt");
+//     File f1 = f;
+//     f1.write("Sagar");
+//     for(char c : f1.fileToByteVec()){
+//         cout << c;
+//     }
+//     cout << "\n";
+//     randonFunction(f1);
+//     cout << "basicTest() ended.\n";
+// }
+// int main(){
+//     cout << "======================= File Manager Creation/Deletion =======================\n";
+//     basicTest();
+//     cout << "==============================================================================\n";
+//     return 0;
+// }
--- Item-14/Server.cpp
+++ Item-14/Server.cpp
@@ -1,7 +1,10 @@
+/**
+ * \brief Item 14: Think carefully about copying behaviour in resource-managing classes.
+ */
+
 #include <iostream>
 #include <boost/asio.hpp>
 #include <fstream>
-#include <vector>
 #include "FileManager.cpp"
 
 #define PORT_NUMBER 1024
@@ -10,43 +13,43 @@ using namespace boost::asio;
 using boost::asio::ip::udp;
 using namespace std;
 
-class Client{
+class Server{
 public:
-    Client() : m_udpSocket(m_ioContext){
-        cout << "Client::Client()\n";
+    Server() : m_udpSocket(m_ioContext){
+        cout << "Server::Server()\n";
 
-        initiateSocketConn();     
+        initiateSocketConn();
     }
-    ~Client(){
-        cout << "~Client::Client()\n";
+    ~Server(){
+        cout << "Server::~Server()\n";
         if(m_udpSocket.is_open())
             m_udpSocket.close();
-    }    
+    }
 
-    // Blocking function
-    void sendByteData(std::vector<char> byteData){
-
+     void receiveByteData(std::vector<char> &fileData){
         if(!m_udpSocket.is_open()){
-            cout << "Client: Socket not open for sharing data.\n";
+            cout << "Server: Socket not open for sharing data.\n";
             return;
         }
         else{
             try{
+                cout << "Server: Waiting for data from client...\n";
                 boost::system::error_code err;
-                m_udpSocket.send_to(boost::asio::buffer(byteData, byteData.size()), m_serverEndpoint, 0, err);
+                int bytesRead = m_udpSocket.receive_from(boost::asio::buffer(fileData, fileData.size()), m_endpoint, 0, err);
 
                 if(err)
                     throw std::runtime_error(err.message());
                 else{
-                    cout << "Client: Message sent.\n";                    
+                    fileData.resize(bytesRead);
+                    std::cout << "Server: Received " << bytesRead << " bytes from " << m_endpoint << "\n";
                 }
             }catch(std::exception &ex){
-                cout << "Client: Failed to send message to the localhost with port " << PORT_NUMBER;
+                cout << "Server: Failed to receive message to the localhost with port " << PORT_NUMBER;
             cout << ": " << ex.what() << endl;
             }
 
             m_udpSocket.close();
-        }
+        }    
     }
 
 private:
@@ -54,7 +57,7 @@ private:
     boost::asio::io_context m_ioContext;
 
     // Server endpoint
-    boost::asio::ip::udp::endpoint m_serverEndpoint;
+    boost::asio::ip::udp::endpoint m_endpoint;
 
     // UDP Socket
     boost::asio::ip::udp::socket m_udpSocket;
@@ -65,14 +68,14 @@ private:
             boost::system::error_code err;
 
             // Initialise the server endpoint
-            m_serverEndpoint.address(boost::asio::ip::address_v4::any());
-            m_serverEndpoint.port(PORT_NUMBER);
+            m_endpoint.address(boost::asio::ip::address_v4::any());
+            m_endpoint.port(PORT_NUMBER);
 
             // Opens a socket without binding to a specific port.
             m_udpSocket.open(udp::v4(), err);
 
             // Binding the socket to the localhost & the specified port number
-            m_udpSocket.bind(m_serverEndpoint, err);
+            m_udpSocket.bind(m_endpoint, err);
 
             if(err){
                 throw std::runtime_error(err.message());
@@ -86,15 +89,18 @@ private:
             cout << ": " << ex.what() << endl;
         }
     }
+
+   
 };
 
-int main(int argc, char *argv[]){
-    
-    File file("/home/ssutar/Tutorials/Effective-CPP/Item-14/client.txt");
-    file.write("Stuff written by Client.\n");
+int main()
+{
+    std::vector<char> receivedStrBuffer(100);
+    Server obj;
+    obj.receiveByteData(receivedStrBuffer);
 
-    // Client obj;
-    // obj.sendByteData(file.fileToByteVec());
+    File file("server.txt", std::ios::out);
+    file.write(std::string(receivedStrBuffer.begin(), receivedStrBuffer.end()));
 
     return 0;
 }
